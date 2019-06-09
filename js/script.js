a1419c130ea351d689a70dabd57e27812add1750
Frozenzxc <39233749+Frozenzxc@users.noreply.github.com>
2019-06-09 21:46:05 +0300

    added modal animation

--- js/script.js
+++ js/script.js
@@ -1,28 +1,72 @@
 var modalLetter = document.querySelector("#modal-letter");
-var modalOverlay = document.querySelector("#modal-overlay");
+var modalLetterOverlay = document.querySelector("#modal-letter-overlay");
+var modalMapOverlay = document.querySelector("#modal-map-overlay");
 var modalLetterOpenButton = document.querySelector("#modal-letter-open");
 var modalLetterCloseButton = document.querySelector("#modal-letter-close");
 var modalMap = document.querySelector("#modal-map");
 var modalMapOpenButton = document.querySelector("#modal-map-open");
 var modalMapCloseButton = document.querySelector("#modal-map-close");
 
-modalLetterCloseButton.addEventListener("click", function() {
+var form = modalLetter.querySelector("#modal-letter-form");
+var name = modalLetter.querySelector("#modal-name");
+var email = modalLetter.querySelector("#modal-email");
+var coverLetter = modalLetter.querySelector("#modal-cover-letter");
+
+modalLetterCloseButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
     modalLetter.classList.toggle("modal-close");
-    modalOverlay.classList.toggle("modal-close");
+    modalLetterOverlay.classList.toggle("modal-close");
 });
 
-modalLetterOpenButton.addEventListener("click", function() {
+modalLetterOpenButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
     modalLetter.classList.toggle("modal-close");
-    modalOverlay.classList.toggle("modal-close");
+    modalLetterOverlay.classList.toggle("modal-close");
 });
 
-modalMapCloseButton.addEventListener("click", function() {
+modalMapCloseButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
     modalMap.classList.toggle("modal-close");
-    modalOverlay.classList.toggle("modal-close");
+    modalMapOverlay.classList.toggle("modal-close");
 });
 
-modalMapOpenButton.addEventListener("click", function () {
+modalMapOpenButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
     modalMap.classList.toggle("modal-close");
-    modalOverlay.classList.toggle("modal-close");
+    modalMapOverlay.classList.toggle("modal-close");
 });
 
+modalLetterOverlay.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalLetter.classList.toggle("modal-close");
+    modalLetterOverlay.classList.toggle("modal-close");
+});
+
+modalMapOverlay.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalMap.classList.toggle("modal-close");
+    modalMapOverlay.classList.toggle("modal-close");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (!modalLetter.classList.contains("modal-close")) {
+            modalLetter.classList.add("modal-close");
+            modalLetterOverlay.classList.add("modal-close");
+        }
+        else if (!modalMap.classList.contains("modal-close")) {
+            modalMap.classList.add("modal-close");
+            modalMapOverlay.classList.add("modal-close");
+        }
+    }
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!name.value || !email.value || !coverLetter.value) {
+        evt.preventDefault();
+        modalLetter.classList.remove("modal-error");
+        /*modalLetter.offsetWidth = modalLetter.offsetWidth;*/
+        modalLetter.classList.add("modal-error");
+    }
+});
