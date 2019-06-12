c015249b486cd27c86b0f05cccca286c6e7922ac
Frozenzxc <39233749+Frozenzxc@users.noreply.github.com>
2019-06-12 22:43:32 +0300

    added sliders

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//управление состоянием модальных окон
+
 var modalLetter = document.querySelector("#modal-letter");
 var modalLetterOverlay = document.querySelector("#modal-letter-overlay");
 var modalMapOverlay = document.querySelector("#modal-map-overlay");
@@ -12,52 +14,50 @@ var name = modalLetter.querySelector("#modal-name");
 var email = modalLetter.querySelector("#modal-email");
 var coverLetter = modalLetter.querySelector("#modal-cover-letter");
 
+var modal = function(modalObj, modalOverlay) {
+    modalObj.classList.toggle("modal-close");
+    modalOverlay.classList.toggle("modal-close");
+};
+
+
 modalLetterCloseButton.addEventListener("click", function(evt) {
     evt.preventDefault();
-    modalLetter.classList.toggle("modal-close");
-    modalLetterOverlay.classList.toggle("modal-close");
+    modal(modalLetter, modalLetterOverlay);
 });
 
 modalLetterOpenButton.addEventListener("click", function(evt) {
     evt.preventDefault();
-    modalLetter.classList.toggle("modal-close");
-    modalLetterOverlay.classList.toggle("modal-close");
+    modal(modalLetter, modalLetterOverlay);
 });
 
 modalMapCloseButton.addEventListener("click", function(evt) {
     evt.preventDefault();
-    modalMap.classList.toggle("modal-close");
-    modalMapOverlay.classList.toggle("modal-close");
+    modal(modalMap, modalMapOverlay);
 });
 
 modalMapOpenButton.addEventListener("click", function (evt) {
     evt.preventDefault();
-    modalMap.classList.toggle("modal-close");
-    modalMapOverlay.classList.toggle("modal-close");
+    modal(modalMap, modalMapOverlay);
 });
 
 modalLetterOverlay.addEventListener("click", function (evt) {
     evt.preventDefault();
-    modalLetter.classList.toggle("modal-close");
-    modalLetterOverlay.classList.toggle("modal-close");
+    modal(modalLetter, modalLetterOverlay);
 });
 
 modalMapOverlay.addEventListener("click", function (evt) {
     evt.preventDefault();
-    modalMap.classList.toggle("modal-close");
-    modalMapOverlay.classList.toggle("modal-close");
+    modal(modalMap, modalMapOverlay);
 });
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
         if (!modalLetter.classList.contains("modal-close")) {
-            modalLetter.classList.add("modal-close");
-            modalLetterOverlay.classList.add("modal-close");
+            modal(modalLetter, modalLetterOverlay);
         }
         else if (!modalMap.classList.contains("modal-close")) {
-            modalMap.classList.add("modal-close");
-            modalMapOverlay.classList.add("modal-close");
+            modal(modalMap, modalMapOverlay);
         }
     }
 });
@@ -66,7 +66,87 @@ form.addEventListener("submit", function (evt) {
     if (!name.value || !email.value || !coverLetter.value) {
         evt.preventDefault();
         modalLetter.classList.remove("modal-error");
-        /*modalLetter.offsetWidth = modalLetter.offsetWidth;*/
         modalLetter.classList.add("modal-error");
     }
+});
+
+
+//слайдеры
+
+var slider = function (arrDots, arrSlides, timer) {
+    var nextDot;
+    var nextSlide;
+    nextDot = arrDots[timer];
+    nextSlide = arrSlides[timer];
+    currentDot.classList.remove("counter-btn-active");
+    currentSlide.classList.remove("slider-item-active");
+    nextDot.classList.add("counter-btn-active");
+    nextSlide.classList.add("slider-item-active");
+    currentDot = nextDot;
+    currentSlide = nextSlide;
+};
+
+//промо слайдер(верхний)
+
+var sliderItems = document.querySelectorAll(".slider-item");
+var sliderButtons = document.querySelectorAll(".counter-btn");
+var currentDot = document.querySelector(".counter-btn-active");
+var currentSlide = document.querySelector(".slider-item-active");
+
+var currentIndex = function () {
+    var array = Array.from(sliderButtons);
+    return  array.indexOf(currentDot) + 1;
+};
+
+
+
+var sliderCounter = document.querySelector('.slider-counter');
+var dotSlider = function(evt) {
+        var counter = evt.dataset.id;
+        var btnActive = "counter-btn-active";
+        var sliderActive = "slider-item-active";
+        slider(sliderButtons, sliderItems, counter);
+    };
+
+sliderCounter.addEventListener('click', function(evt) {
+    var dot = evt.target.closest('.counter-btn');
+
+    dotSlider(dot);
+});
+
+var sliderTimer = function() {
+    var counter = currentIndex();
+    if(counter < sliderButtons.length) {
+        slider(sliderButtons, sliderItems, counter);
+    }
+    else {
+        counter = 0;
+        slider(sliderButtons, sliderItems, counter);
+    }
+};
+
+setInterval(sliderTimer,2000);
+
+//слайдер услуг(нижний)
+
+var servicesButtons = document.querySelectorAll(".main-services-link-active");
+var servicesSlides = document.querySelectorAll(".services-description-item");
+var currentServicesBtn = document.querySelector(".main-services-link-active");
+var currentservicesSlide = document.querySelector(".services-description-item-active");
+
+var servicesCounter = document.querySelector('.main-services-list');
+var servicesSlider = function(evt) {
+    var counter = evt.dataset.id;
+    var nextSlide;
+    nextSlide = servicesSlides[counter];
+    currentservicesSlide.classList.remove("services-description-item-active");
+    nextSlide.classList.add("services-description-item-active");
+    currentservicesSlide = nextSlide;
+
+};
+
+servicesCounter.addEventListener('click', function(evt) {
+    var dot = evt.target.closest('.main-services-link');
+
+    servicesSlider(dot);
 });
